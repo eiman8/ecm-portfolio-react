0605f77bd4812f345bfea5f2df9c0461f929bbc3
Eimanson Maybituin <ecmabta8@gmail.com>
2025-10-02 07:58:24 +0800

    first commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,27 @@
+
+function HeroSection() {
+    return (
+        <section id="heroSection" className="hero-section">
+            <div className="hero-section-content-box">
+                <div className="hero-section-content">
+                    <p className="section-title"> Hey, I'm Eiman</p>
+                    <h1 className="hero-section-title"><span className="hero-section-title-color">Full Stack</span>{" "}
+                    <br />
+                    Developer
+                    </h1>
+                    <p className="hero-section-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        <br />
+                        Rerum, laudantium saepe. Est, excepturi voluptates!
+                    </p>
+                </div>
+                <button className="btn btn-primary">Get in Touch</button>
+            </div>
+            <div className="hero-section-image">
+                <img src="./img/hero_img.png" alt="Hero Section" />
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection;
