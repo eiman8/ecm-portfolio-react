7243c0cf7109de957db9001ec0c2af55a27bc839
Eimanson Maybituin <eimansonmaybituin@Eimansons-MacBook-Pro.local>
2025-10-02 15:28:34 +0800

    modified hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -4,21 +4,23 @@ function HeroSection() {
         <section id="heroSection" className="hero-section">
             <div className="hero-section-content-box">
                 <div className="hero-section-content">
-                    <p className="section-title"> Hey, I'm Eiman</p>
-                    <h1 className="hero-section-title"><span className="hero-section-title-color">Full Stack</span>{" "}
+                    <p className="section-title"> Hey, I'm Eimanson</p>
+                    <h1 className="hero-section-title"><span className="hero-section-title-color">Website</span>{" "}
                     <br />
                     Developer
                     </h1>
                     <p className="hero-section-description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. */}
+                        An aspiring web developer which aims to build beautiful and responsive web designs. 
                         <br />
-                        Rerum, laudantium saepe. Est, excepturi voluptates!
+                        <br />
+                        Get to know more about me.
                     </p>
                 </div>
                 <button className="btn btn-primary">Get in Touch</button>
             </div>
             <div className="hero-section-image">
-                <img src="./img/hero_img.png" alt="Hero Section" />
+                {/* <img src="./img/hero_img.png" alt="Hero Section" /> */}
             </div>
         </section>
     )
